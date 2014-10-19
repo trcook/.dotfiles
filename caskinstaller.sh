f7acfe380d7143b8ab2a977366c3c447f70b1748
Thomas Cook <trcook@gmail.com>
2014-10-18 18:55:04 -0600

    updated configuration instructions

--- caskinstaller.sh
+++ caskinstaller.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-# run brewinstaller first
+# on error -- stop: run ./brewinstaller.sh first
 brew cask install alfred
-brew cask install sublime-text
 brew cask install dash
 brew cask install rstudio
 brew cask install dropbox
 brew cask install sourcetree
 brew cask install iterm2
 brew cask install pandoc
+brew cask install textmate
